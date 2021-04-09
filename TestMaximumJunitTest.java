b8d534de81742631d1de41909297107c9f7d6660
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-09 20:05:30 +0530

    ADD: test cases for 3 strings

--- TestMaximumJunitTest.java
+++ TestMaximumJunitTest.java
@@ -5,22 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class TestMaximumJunitTest {
 	
-    @Test
+@Test
     void GivenString_MaxNumber_FirstPosition_ReturnsTrue_IfSameAsExpected(){
         MaximumNumber maxString = new MaximumNumber();
-        String max = maxString.getMaxString("Banana", "Apple", "Peach");
+        String max = maxString.getMaxString("Peach", "Apple", "Banana");
         Assertions.assertEquals( "Peach", max);
     }
     @Test
     void GivenString_MaxNumber_SecondPosition_ReturnsTrue_IfSameAsExpected(){
         MaximumNumber maxString = new MaximumNumber();
-        String max = maxString.getMaxString("Apple","Banana", "Peach");
+        String max = maxString.getMaxString("Apple","Peach", "Banana");
         Assertions.assertEquals( "Peach", max);
     }
     @Test
     void GivenString_MaxNumber_ThirdPosition_ReturnsTrue_IfSameAsExpected(){
         MaximumNumber maxString = new MaximumNumber();
-        String max = maxString.getMaxString("Peach", "Apple", "Banana");
+        String max = maxString.getMaxString("Banana", "Apple", "Peach");
         Assertions.assertEquals( "Peach", max);
     }
 }
